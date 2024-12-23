2f449e4d6fe64c769962ccc7c1131fba588e9043
jocuartasp <jocuartasp@unal.edu.co>
2024-12-23 15:52:51 -0500

    change: Implementacion de credenciales con laravel breeze y implementacion de plantilla gratuita con sus respectivas rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,57 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+#Rutas estaticas
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/appointment', function () {
+    return view('appointment');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/opening', function () {
+    return view('opening');
+});
+
+Route::get('/price', function () {
+    return view('price');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+
+require __DIR__.'/auth.php';
